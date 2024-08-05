279ad02ae129f3869830202a088099ffe8aba128
developerchoims <developerchoims@gmail.com>
2024-08-05 23:13:47 +0900

    커밋테스트

--- hoopi/src/main/java/com/ms/hoopi/controller/JobController.java
+++ hoopi/src/main/java/com/ms/hoopi/controller/JobController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("hoopi")
+@RestController
+@RequestMapping("hoopi")
 public class JobController {
     @Autowired
     private final JobService jobService;
