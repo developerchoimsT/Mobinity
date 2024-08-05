1119770f77b732789d64c562ae94b3c6aae90153
developerchoims <developerchoims@gmail.com>
2024-08-06 01:27:28 +0900

    커밋테스트

--- hoopifront/src/userInfo.js
+++ hoopifront/src/userInfo.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const UserInfo = async () =>{
+const UserInfo = () =>{
     try {
         const response = axios.get("http://hoopi.p-e.kr/api/hoopi/userInfo");
-        console.log(response);
+        console.log(response.data);
         return response.data;
     } catch(error) {
         console.log("유저 정보를 불러오지 못했습니다.", error);
