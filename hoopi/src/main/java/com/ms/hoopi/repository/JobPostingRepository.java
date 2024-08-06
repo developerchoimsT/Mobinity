edce21364ba0d1249f53e95a0cddb94b1583eed2
developerchoims <developerchoims@gmail.com>
2024-08-06 10:31:06 +0900

    커밋테스트

--- hoopi/src/main/java/com/ms/hoopi/repository/JobPostingRepository.java
+++ hoopi/src/main/java/com/ms/hoopi/repository/JobPostingRepository.java
@@ -1,10 +1,9 @@
 package com.ms.hoopi.repository;
 
 import com.ms.hoopi.model.dto.JobPostingDto;
-import com.ms.hoopi.model.entity.Company;
+import com.ms.hoopi.model.entity.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobPostingRepository extends JpaRepository<Company, Integer> {
+public interface JobPostingRepository extends JpaRepository<JobPosting, Integer> {
 
-    void save(JobPostingDto jobPostingDto);
 }
