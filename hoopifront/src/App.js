dc87dbae3bfc2ad237bb564458dff20b7a122249
developerchoims <developerchoims@gmail.com>
2024-08-06 16:42:55 +0900

    커밋테스트

--- hoopifront/src/App.js
+++ hoopifront/src/App.js
@@ -4,65 +4,55 @@ import Join from './Join';
 import Login from './Login';
 import JobPosting from "./JobPosting";
 import PostJobs from "./PostJobs";
-import {useEffect, createContext, useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
-// Context 생성
-const UserContext = createContext(null);
 
-// 컨텍스트 프로바이더 컴포넌트
-function UserProvider({ children }) {
-    const [userId, setUserId] = useState('');
+function App() {
+    const [usersId, setUsersId] = useState('');
+    const navigate = useNavigate();
 
     useEffect(() => {
-        async function fetchUserInfo() {
+        const fetchUserInfo = async () => {
             try {
-                const response = await axios.get('http://hoopi.p-e.kr/api/hoopi/userInfo');
-                if (response.data !== "" && response.data != null) {
-                    console.log(response.data);
-                    setUserId(response.data);
+                const res = await axios.get("http://hoopi.p-e.kr/api/hoopi/userInfo");
+                if(res.data !== "" && res.data != null){
+                    console.log(res.data); // 첫 로딩 시 콘솔 출력
+                    setUsersId(res.data);
                 }
             } catch (error) {
                 console.error("유저 정보를 불러오지 못했습니다.", error);
             }
-        }
+        };
 
         fetchUserInfo();
     }, []);
 
+    useEffect(() => {
+        console.log(usersId); // usersId가 변경될 때마다 콘솔에 출력
+    }, [usersId]);
+
     return (
-        <UserContext.Provider value={{ userId, setUserId }}>
-            {children}
-        </UserContext.Provider>
+        <Router>
+            <nav className='mainNav'>
+                <ul>
+                    <li><Link to='/jobPosting'>채용 공고</Link></li>
+                </ul>
+                <ul>
+                    <li><Link to="/join">회원가입</Link></li>
+                    <li><Link to="/login">로그인</Link></li>
+                </ul>
+            </nav>
+            <div className='main-box'>
+                <Routes>
+                    <Route path='/join' element={<Join />}/>
+                    <Route path='/login' element={<Login />}/>
+                    <Route path='/jobPosting' element={<JobPosting />}/>
+                    <Route path='/' element={<JobPosting />}/>
+                    <Route path='/postJobs' element={<PostJobs usersId={usersId}/>}/>
+                </Routes>
+            </div>
+        </Router>
     );
-}
-
-function App() {
-
-  return (
-
-      <UserProvider>
-          <Router>
-              <nav className='mainNav'>
-                  <ul>
-                      <li><Link to='/jobPosting'>채용 공고</Link></li>
-                  </ul>
-                  <ul>
-                      <li><Link to="/join">회원가입</Link></li>
-                      <li><Link to="/login">로그인</Link></li>
-                  </ul>
-              </nav>
-              <div className='main-box'>
-                  <Routes>
-                      <Route path='/join' element={<Join />}/>
-                      <Route path='/login' element={<Login />}/>
-                      <Route path='/jobPosting' element={<JobPosting />}/>
-                      <Route path='/' element={<JobPosting />}/>
-                      <Route path='/postJobs' element={<PostJobs />}/>
-                  </Routes>
-              </div>
-          </Router>
-      </UserProvider>
-  );
 }
 
 export default App;
