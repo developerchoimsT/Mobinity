a45e7985966e88df7d3b57370af2ea9dbb06344f
developerchoims <developerchoims@gmail.com>
2024-08-12 20:16:50 +0900

    커밋테스트

--- hoopifront/src/PostJobs.js
+++ hoopifront/src/PostJobs.js
@@ -1,4 +1,4 @@
-import {useContext, useState} from "react";
+import {useContext, useState, useEffect} from "react";
 import axios from "axios";
 import {useLocation, useNavigate} from "react-router-dom";
 import {UserContext} from "./App";
