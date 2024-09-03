4ffc4f8d192128cf61d9cc5386f64bf7601da97f
developerchoims <developerchoims@gmail.com>
2024-09-03 12:22:33 +0900

    0903-hoopi시작(로그인, 회원가입, 메인 페이지 구현 시작)

--- hoopi/src/main/java/com/ms/hoopi/repository/IpRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ms.hoopi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IpRepository extends JpaRepository<Ip, String> {
-}
