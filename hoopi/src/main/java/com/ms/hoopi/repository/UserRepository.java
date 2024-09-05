329bda62568dd66a48cf278fe312fbf63760edaf
developerchoims <developerchoims@gmail.com>
2024-09-05 14:52:13 +0900

    0903- join - 데이터 user테이블, address테이블에 나뉘어 저장하도록 설정

--- hoopi/src/main/java/com/ms/hoopi/repository/UserRepository.java
+++ hoopi/src/main/java/com/ms/hoopi/repository/UserRepository.java
@@ -2,13 +2,15 @@ package com.ms.hoopi.repository;
 
 import com.ms.hoopi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByUserId(String id);
     User save(User user);
-    Optional<User> findById(String Id);
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findById(@Param("id") String id);
 }
