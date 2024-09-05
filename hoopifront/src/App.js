149f81d932407d13dc8ec09264184b23f29feca4
developerchoims <developerchoims@gmail.com>
2024-09-06 00:45:59 +0900

    0906 - login 로직 수정, jwtFilter작동 방식 수정(프론트에서 interceptor로 jwt검사, 401발생시 refreshToken 검사하여 acsToken생성)

--- hoopifront/src/App.js
+++ hoopifront/src/App.js
@@ -6,7 +6,7 @@ import Join from './Join';
 import './App.css';
 import Main from "./main/Main";
 import Search from "./search/Search";
-import './axiosInterceptor';
+import 'axios/axiosInterceptor.js';
 
 // Context 생성
 const UserContext = createContext(null);
