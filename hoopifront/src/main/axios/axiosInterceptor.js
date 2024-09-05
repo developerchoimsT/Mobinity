8afd24f58c59deac31a658651ef491cadd9b485b
developerchoims <developerchoims@gmail.com>
2024-09-06 01:15:30 +0900

    0906 - login 로직 수정, jwtFilter작동 방식 수정(프론트에서 interceptor로 jwt검사, 401발생시 refreshToken 검사하여 acsToken생성)

--- hoopifront/src/main/axios/axiosInterceptor.js
+++ hoopifront/src/main/axios/axiosInterceptor.js
@@ -12,18 +12,27 @@ axios.interceptors.response.use(
       if ((error.response.status === 401 || error.response.status === 403) && !originalRequest._retry) {
         originalRequest._retry = true;
 
-        // localStorage에서 user ID 가져오기
-        const id = localStorage.getItem("id");
-
         try {
-          // 서버로 userId를 보내서 Redis에 있는 Refresh Token으로 새로운 Access Token 요청
-          await axios.post('http://hoopi.p-e.kr/api/hoopi/refresh-token', { id });
+          // localStorage에서 user ID 가져오기
+          const id = localStorage.getItem("id");
 
-          // 서버가 새 Access Token을 쿠키에 저장했으므로 원래 요청을 다시 시도
-          return axios(originalRequest);
-        } catch (error) {
-          console.error('Refresh token request failed:', error);
-          window.location.reload('/');
+          // 서버로 userId를 보내서 Redis에 있는 Refresh Token으로 새로운 Access Token 요청
+          const tokenResponse = await axios.post('http://hoopi.p-e.kr/api/hoopi/refresh-token', { id });
+
+          if (tokenResponse.status === 200) {
+            // 새 Access Token을 쿠키에 저장한 후 원래 요청을 다시 시도
+            return axios(originalRequest);
+          } else {
+            // 리프레시 토큰 요청 실패 시
+            console.error('Failed to refresh token');
+            window.location.reload('/'); // 로그인 페이지로 리디렉션
+            return Promise.reject(error);
+          }
+
+        } catch (refreshError) {
+          console.error('Refresh token request failed:', refreshError);
+          window.location.reload('/'); // 로그인 페이지로 리디렉션
+          return Promise.reject(refreshError);
         }
       }
 
