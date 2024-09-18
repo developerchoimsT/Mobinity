5dba35d6d51e142714663d7f97f12a9cc93fdcda
developerchoims <developerchoims@gmail.com>
2024-09-19 00:15:57 +0900

    0918 - 게시글 작성 로직 수정

--- hoopi/src/main/java/com/ms/hoopi/admin/article/controller/ArticleController.java
+++ hoopi/src/main/java/com/ms/hoopi/admin/article/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.ms.hoopi.admin.article.model.ArticleRequestDto;
 import com.ms.hoopi.admin.article.model.ProductRequestDto;
 import com.ms.hoopi.admin.article.servie.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("hoopi/")
@@ -21,7 +23,9 @@ public class ArticleController {
 
     @PostMapping("/article")
     public ResponseEntity<String> addArticle(@RequestParam("imgs")List<MultipartFile> imgs
-                                            , @RequestParam("data") ArticleCreationRequestDto request) throws IOException {
-        return articleService.addArticle(imgs, request.getProduct(), request.getArticle());
+                                            , @RequestParam("data") ArticleCreationRequestDto data) throws IOException {
+        log.info("imgs:{}", imgs);
+        log.info("data:{}", data);
+        return articleService.addArticle(imgs, data.getProduct(), data.getArticle());
     }
 }
