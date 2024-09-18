42c5ff1289a5e531d25168a06189baf5a69d0580
developerchoims <developerchoims@gmail.com>
2024-09-18 12:01:41 +0900

    0917 - 메뉴 구현, 회원 탈퇴(관리자) 구현

--- hoopifront/src/admin/user/userBody.js
+++ hoopifront/src/admin/user/userBody.js
@@ -51,7 +51,7 @@ const UserBody = () => {
 
     const handleUserQuit = async (e) => {
         try {
-            const response = await axios.put('http://hoopi.p-e.kr/api/hoopi/admin/user-quit', {params: {userId: e.target.id}});
+            const response = await axios.put('http://hoopi.p-e.kr/api/hoopi/admin/user-quit', {params: {userId: userDetail.id}});
             alert("사용자 탈퇴 처리가 완료되었습니다.");
             fetchUsers(currentPage); // 상태 업데이트 후 목록 새로고침
         } catch (error) {
